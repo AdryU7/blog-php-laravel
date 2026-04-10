300674d845464e15d0f9d43ed22ef89541c195ad
AdryU7 <adri_francis2003@outlook.com>
2026-04-10 14:02:54 -0500

    Activity #1: Tech Company case

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    /*Si en caso de usar "fillable" en lugar de "guarded", seç
+    /*Si en caso de usar "fillable" en lugar de "guarded", se
     tiene que definir los campos que van a permitir poder 
     asignarse de forma masiva.
     */
--- database/migrations/2026_03_17_222149_create_profiles_table.php
+++ database/migrations/2026_03_17_222149_create_profiles_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('photo', 255)->nullable();
+            // ACTIVIDAD Nº1
+            $table->string('profession', 60)->nullable();
+            $table->string('about', 255)->nullable();
+            $table->string('twitter', 100)->nullable();
+            $table->string('linkedin', 100)->nullable();
+            $table->string('facebook', 100)->nullable();
 
             //1ra forma
             /*$table->unsignedBigInteger('user_id')->unique();
